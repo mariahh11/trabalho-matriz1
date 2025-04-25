fae396b6062a278eed26ddf22a74248f3e20b4c1
mariahh11 <160670733+mariahh11@users.noreply.github.com>
2025-04-24 21:33:02 -0300

    Update trabalho_matriz1.c
    Menu, case 1 e printf teste

---
+++ trabalho_matriz1.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+int main(){
+    int m, n;
+    int opcao;
+    int count = 0;
+
+    //Leitura da matriz
+    printf("Primeiro, precisamos da quantidade de linhas e colunas da matriz.\n");
+    printf("Linhas: ");
+    scanf("%d", &m);
+    printf("Colunas: ");
+    scanf("%d", &n);
+
+    //Todos os elementos da matriz são 0
+    int matriz[m][n];
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            matriz[i][j] = 1;
+        }
+    }
+
+    //Loop do menu
+    do{
+    
+    //Tela de menu (podemos colocar todas as opções em um printf separado, fica um pouco mais feio mas acho mais simples deixar em um printf só)
+    printf("1 - Criar um novo conjunto vazio\n2 - Inserir dados em um conjunto\n3 - Remover um conjunto\n4 - Fazer união entre dois conjuntos\n5 - Fazer a intersecção entre dois conjuntos\n6 - Mostrar um conjunto\n7 - Mostrar todos os conjuntos\n8 - Busca por um valor\n9 - Sair do programa\n");
+
+    printf("Opção: ");
+    scanf("%d", &opcao);
+
+    switch (opcao){
+        case 1:{
+            //+ conjunto VAZIO
+            if (count >= m){
+                //Caso o usuário queira adicionar mais de m conjuntos, o programa aparece uma mensagem de erro
+                printf("ERRO - Número máximo de conjuntos atingido");
+                break;
+            }
+
+            //Criação do conjunto vazio
+            printf("Vamos criar um novo conjunto vazio!\n");
+            int conjunto[count];
+            for (int i = 0; i < n; i++){
+                conjunto[count] = 0;
+                matriz[count][i] = conjunto[count];
+            }
+            count += 1;
+            break;
+        }
+
+        case 2:
+
+        default:
+            //Caso o usuário digite uma opção inválida - mensagem de erro
+            printf("ERRO - Opção inválida. Tente novamente.\n");
+    }
+    } while (opcao != 9);
+
+
+
+
+    //Imprimir a matriz final, usando pra testar o código.
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+    printf("Count: %d", count);
+
+    return 0;
+}
